b293dab46aa27cffd3b41637c4e571c66d5d51e2
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-04-09 11:17:36 +0200

    up with you

--- src/components/Container.test.jsx
+++ src/components/Container.test.jsx
@@ -30,7 +30,7 @@ jest.mock('axios', () => {
 // 4- wrapper exposes different methods that look alike!!!!
 //    - get will crash and burn fast if no match
 //    - query will return null if no match
-
+//    - find will wait, and only fail after 5s
 describe('Container', () => {
   // actual tests
   it('outputs "hello world"', () => {
@@ -41,5 +41,19 @@ describe('Container', () => {
   it('does not output "loading..."', () => {
     const wrapper = rtl.render(<Container lady='gaga' />);
     expect(wrapper.queryByText(/loading/i)).toBeFalsy();
+  });
+
+  it('renders quotes texts', async () => {
+    const wrapper = rtl.render(<Container />);
+    // quotes not there
+    expect(wrapper.queryByText(/mentored/i)).toBeFalsy();
+    // fire event that will bring quotes
+    rtl.fireEvent.click(wrapper.getByText(/get those/i));
+    // await for certain quote to be there
+    await wrapper.findByText(/mentored/i);
+    // now I can run my assertions comfortably
+    expect(wrapper.getByText(/mentored/i));
+    expect(wrapper.getByText(/fun/i));
+    expect(wrapper.getByText(/network/i));
   });
 });
