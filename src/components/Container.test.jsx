c45e9389d1b277991c39270b0f873f26a98c3805
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-04-09 10:26:19 +0200

    second test

--- src/components/Container.test.jsx
+++ src/components/Container.test.jsx
@@ -7,7 +7,11 @@ describe('Container', () => {
   // actual tests
   it('outputs "hello world"', () => {
     const wrapper = rtl.render(<Container />);
-    // console.log(wrapper.debug())
     expect(wrapper.getByText('Hello, World!'));
+  });
+
+  it('does not output "loading..."', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.queryByText('Loading...')).toBeFalsy();
   });
 });
