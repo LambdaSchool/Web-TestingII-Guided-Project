fab8049bd880ebb21e17b713b2a7b883954cc864
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-04-09 10:45:15 +0200

    complicate container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Container.less';
 
+// fetch quotes from a quotes api and display them
 export default class Container extends Component {
+  state = { quotes: [] }
+
+  getQuotes = () => {
+    axios.get('https://quotes.org/api')
+      .then(res => {
+        this.setState({ quotes: res.data });
+      });
+  }
+
   render() {
     return (
       <div className='container-hello-world'>
