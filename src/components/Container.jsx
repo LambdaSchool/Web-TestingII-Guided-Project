ae9c9304e2ee00d9241ffa204bf10a9a3a86b933
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-04-09 10:47:49 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './Container.less';
 
 // fetch quotes from a quotes api and display them
+// { id: '123', text: 'Use postman!!' }
+
 export default class Container extends Component {
   state = { quotes: [] }
 
@@ -17,6 +19,16 @@ export default class Container extends Component {
     return (
       <div className='container-hello-world'>
         Hello, World! {this.props.lady}
+
+        {
+          this.state.quotes.map(q => (
+            <div key={q.id}>{q.text}</div>
+          ))
+        }
+
+        <button onClick={this.getQuotes}>
+          get those quotes
+        </button>
       </div>
     );
   }
